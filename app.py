c4a3d69eaa78a9d6d3e8c5ec82ff500d3150c8e2
daanniill <daniil.malakhov12@gmail.com>
2025-03-23 14:49:56 -0700

    website implemented, some bugs with text not appearing on screen, most likely behind the img

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 def get_meme():
-    url = "https://meme-api.com"
+    url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
